c5844963d80e19ccd249176969a49a58ae9dd035
Laurie Fish <iamlauriefish@gmail.com>
2023-01-15 15:49:34 -0800

    added all HTML and 1 event listener

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const italianDropdown = document.getElementById('italian-dd');
+const inputEl = document.getElementById('text-box');
+const buttonEl = document.getElementById('my-button');
+const italianImage = document.getElementById('italian-img');
 /* State */
 
 /* Events */
+italianDropdown.addEventListener('change', () => {
+    italianImage.src = `assets/Italian-${italianDropdown.value}.jpg`;
+});
+
+buttonEl.addEventListener('click', () => {});
+
+inputEl.value = '';
 
 /* Display Functions */
 
