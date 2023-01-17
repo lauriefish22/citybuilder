6916b4d968e435dc9c5989ebb6d547c84c9a3e4c
Laurie Fish <iamlauriefish@gmail.com>
2023-01-16 20:03:42 -0800

    finished event listeners

--- app.js
+++ app.js
@@ -3,12 +3,18 @@
 /* Get DOM Elements */
 const italianDropdown = document.getElementById('italian-dd');
 const mexicanDropdown = document.getElementById('mexican-dd');
+const chineseDropdown = document.getElementById('chinese-dd');
 const inputEl = document.getElementById('text-box');
 const buttonEl = document.getElementById('my-button');
 const italianImage = document.getElementById('italian-img');
 const mexicanImage = document.getElementById('mexican-img');
+const chineseImage = document.getElementById('chinese-img');
 
 /* State */
+let italianCount = 0;
+let mexicanCount = 0;
+let chineseCount = 0;
+// needs HTML spans.
 
 /* Events */
 italianDropdown.addEventListener('change', () => {
@@ -17,6 +23,10 @@ italianDropdown.addEventListener('change', () => {
 
 mexicanDropdown.addEventListener('change', () => {
     mexicanImage.src = `assets/Mexican-${mexicanDropdown.value}.png`;
+});
+
+chineseDropdown.addEventListener('change', () => {
+    chineseImage.src = `assets/Chinese-${chineseDropdown.value}.png`;
 });
 
 buttonEl.addEventListener('click', () => {});
